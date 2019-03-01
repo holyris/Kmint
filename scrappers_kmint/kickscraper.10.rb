bb20d582f3dee7e8af9b5196159234c1dc5a5fca
holyris <alexandre.thebault77@gmail.com>
2019-03-01 17:58:14 +0100

    modification d'affichage

--- scrappers_kmint/kickscraper.10.rb
+++ scrappers_kmint/kickscraper.10.rb
@@ -16,19 +16,19 @@ begin
             
             if page[n][i].country == "FR"
                 
-                puts title = page[n][i].name
-                puts description = page[n][i].blurb
-                puts url = page[n][i].urls.web.project
-                puts img = page[n][i].photo.full
+                title = page[n][i].name
+                description = page[n][i].blurb
+                url = page[n][i].urls.web.project
+                img = page[n][i].photo.full
                 engagement = page[n][i].pledged
                 goal = page[n][i].goal
                 engagement = (engagement*100)/goal
-                puts engagement = engagement.round              #   engagement en pourcentage
+                engagement = engagement.round              #   engagement en pourcentage
                 deadline = Time.at(page[n][i].deadline).to_s    #   convertion en quelque chose de plus lisible        
                 deadline = DateTime.parse(deadline)             #   rend la donnee exploitable 
-                puts timeleft = (deadline - DateTime.now).to_i  #   date de fin moins date actuelle converti en int
-                puts creator = page[n][i].creator.name
-                puts category = page[n][i].category.name
+                timeleft = (deadline - DateTime.now).to_i  #   date de fin moins date actuelle converti en int
+                creator = page[n][i].creator.name
+                category = page[n][i].category.name
                 conn.query("Insert into crowdfunding values ('"+conn.escape(title.to_s)+"','"+conn.escape(description.to_s)+"','"+url.to_s+"','"+img.to_s+"','"+engagement.to_s+"','"+timeleft.to_s+"','"+conn.escape(creator.to_s)+"','"+category.to_s+"')")
                 
 
